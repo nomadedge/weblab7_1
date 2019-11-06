d24ca66b74e7b3dc3dc75506112bad2cb8e6e2a8
nomadedge <nomad.edge@gmail.com>
2019-11-06 15:12:29 +0300

    Refactor code and update README

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 2. SASS - css compiler
 ## Deployment tutorial
 1. Install node.js
-2. Install SASS globaly using command "npm install -g sass"
-3. Run "npm install" to install all dependencies
-4. After every change in scss file use "sass scss/site.scss css/site.css"
+2. Install browserify gobaly using command "npm install -g browserify"
+3. Install SASS globaly using command "npm install -g sass"
+4. Run "npm install" to install all dependencies
+5. Run "browserify main.js -o bundle.js" to create bundle.js
+6. Run "sass scss/site.scss css/site.css" to create css/site.css"
+7. After every change in js files use "browserify main.js -o bundle.js"
+8. After every change in site.scss file use "sass scss/site.scss css/site.css"
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const openWeatherKey = "d5bb735f9e1ce1a846ab736fc9d95dc6";
 const weatherTemplate = Handlebars.compile(document.getElementById('weather-template').innerHTML);
 const errorTemplate = Handlebars.compile(document.getElementById('error-template').innerHTML);
-let globalResult;
 
-async function submit(event) {
+const { getWeather } = require('./weatherGetter');
+
+submit = async event => {
     event.preventDefault();
 
     let lastResult = document.getElementById('result-container');
@@ -15,50 +15,7 @@ async function submit(event) {
     renderResult(result);
 }
 
-function getWeather(cityName) {
-    return new Promise(async result => {
-        const openWeatherUrl =
-            'https://api.openweathermap.org/data/2.5/weather?q=' +
-            cityName +
-            '&appid=' +
-            openWeatherKey;
-
-        const weatherData = await fetch(openWeatherUrl);
-        const weatherJson = await weatherData.json();
-
-        if (weatherData.ok) {
-            const weather = {
-                iconUrl:
-                    'http://openweathermap.org/img/w/' +
-                    weatherJson.weather[0].icon +
-                    '.png',
-                description: weatherJson.weather[0].description,
-                temperature: (weatherJson.main.temp - 273.15).toFixed(0),
-                pressure: weatherJson.main.pressure,
-                humidity: weatherJson.main.humidity,
-                windSpeed: weatherJson.wind.speed
-            };
-            const out = {
-                isOk: true,
-                weather: weather
-            };
-            result(out);
-            return;
-        } else {
-            const out = {
-                isOk: false,
-                error: {
-                    status: weatherJson.cod,
-                    message: weatherJson.message
-                }
-            };
-            result(out);
-            return;
-        };
-    });
-}
-
-function renderResult(result) {
+renderResult = result => {
     let weatherContainer = document.getElementById('weather-container');
     let html;
     if (result.isOk) {
