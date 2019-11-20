7922c7f26e4b3d37b3e0244410c05f53bf247bd3
nomadedge <nomad.edge@gmail.com>
2019-11-20 09:51:18 +0300

    Fix renderResult tests

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,12 +1,8 @@
 const { renderResult, submit } = require('../main');
+const { bodyHtml } = require('../setupJest');
 
 beforeEach(() => {
-    document.body.innerHTML =
-        '<script id="weather-template" type="text/x-handlebars-template"><div id="icon-temp"><img src="{{iconUrl}}" alt="Weather icon"/><div id="temp">{{temperature}}°C</div></div><div><div id="description">{{description}}</div><div>Barometer {{pressure}} hPa</div><div>Humidity {{humidity}} %</div><div>Wind {{windSpeed}} m/s</div></div></script>' +
-        '<script id="error-template" type="text/x-handlebars-template"><div>{{status}}<br>{{message}}</div></script>' +
-        '<div id="weather-container">' +
-        '<form id="city-form"></form>' +
-        '</div>';
+    document.body.innerHTML = bodyHtml;
 });
 
 describe('renderResult function', () => {
--- setupJest.js
+++ setupJest.js
@@ -2,12 +2,16 @@ global.fetch = require('jest-fetch-mock');
 
 global.Handlebars = require('handlebars');
 
-Object.defineProperty(document, 'currentScript', {
-    value: document.createElement('script'),
-});
-document.body.innerHTML =
+const bodyHtml =
     '<script id="weather-template" type="text/x-handlebars-template"><div id="icon-temp"><img src="{{iconUrl}}" alt="Weather icon"/><div id="temp">{{temperature}}°C</div></div><div><div id="description">{{description}}</div><div>Barometer {{pressure}} hPa</div><div>Humidity {{humidity}} %</div><div>Wind {{windSpeed}} m/s</div></div></script>' +
     '<script id="error-template" type="text/x-handlebars-template"><div>{{status}}<br>{{message}}</div></script>' +
     '<div id="weather-container">' +
     '<form id="city-form"></form>' +
     '</div>';
+
+Object.defineProperty(document, 'currentScript', {
+    value: document.createElement('script'),
+});
+document.body.innerHTML = bodyHtml;
+
+exports.bodyHtml = bodyHtml;
